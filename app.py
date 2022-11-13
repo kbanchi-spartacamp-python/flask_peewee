ef1010570a9751ed70abc8f3ffcc196197117f1a
kbanchi1111 <kbanchi.1111@gmail.com>
2022-11-14 01:35:13 +0900

    add flask peewee

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+
+from database import Post
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    posts = Post.select()
+    return render_template("index.html", posts=posts)
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    name = request.form["postName"]
+    title = request.form["postTitle"]
+    body = request.form["postBody"]
+    Post.create(name=name, title=title, body=body)
+    return redirect("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
